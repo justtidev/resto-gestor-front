d91994b06109258b2553236ae523e4c104b9e7d4
justtidev <justynavarro@gmail.com>
2025-09-04 16:13:57 -0300

    import authContext

--- src/admin/categoria/index.jsx
+++ src/admin/categoria/index.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-
+import { AuthContext } from '../contexts/AuthContext';
 import { BiCategory } from "react-icons/bi";
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
 function CategoriaIndex() {
