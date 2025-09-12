566e69058c0b9d50c656ea3f5df3c6718bb5c1c7
justtidev <justynavarro@gmail.com>
2025-09-11 22:36:08 -0300

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import axios from 'axios';
+import { inject } from '@vercel/analytics';
+
+inject();
+
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
